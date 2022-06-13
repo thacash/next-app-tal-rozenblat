74deb9c0cdb2e32038a643d269cade71b433f925
Tal <rozenblattal@gmail.com>
2022-06-13 13:08:08 +0300

    WIP

--- pages/index.js
+++ pages/index.js
@@ -21,10 +21,10 @@ export default function Home({ allPostsData }) {
         <title>{siteTitle}</title>
       </Head>
       <section className={utilStyles.headingMd}>
-        <p>Hello, I'm Tal. I'm a fullstack software developer.
+        <p>Hello, I am Tal. I am a fullstack software developer.
           You can contact me at <a href = "https://www.linkedin.com/in/tal-rozenblat-a389ab207/">LinkedIn</a> </p>
         <p>
-          (This is a sample website - you'll be building a site like this on{' '}
+          (This is a sample website - you will be building a site like this on{' '}
           <a href="https://nextjs.org/learn">our Next.js tutorial</a>.)
         </p>
       </section>
