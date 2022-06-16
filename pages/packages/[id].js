bbdf600d1b3585325ecc23be6c334a7a7f916788
Tal <rozenblattal@gmail.com>
2022-06-16 19:28:55 +0300

    deploy

--- pages/packages/[id].js
+++ pages/packages/[id].js
@@ -4,36 +4,52 @@ import Head from 'next/head';
 import Date from '../../components/date';
 import utilStyles from '../../styles/utils.module.css';
 
-export async function getStaticProps({ params }) {
-    const postData = await getPostData(params.id);
-    return {
-        props: {
-            packageData,
-        },
-    };
-}
+// export async function getStaticProps({ params }) {
+//     const postData = await getPostData(params.id);
+//     return {
+//         props: {
+//             packageData,
+//         },
+//     };
+// }
 
-export async function getStaticPaths() {
-    const paths = getAllPostIds();
-    return {
-        paths,
-        fallback: false,
-    };
-}
+// export async function getStaticPaths() {
+//     const paths = getAllPostIds();
+//     return {
+//         paths,
+//         fallback: false,
+//     };
+// }
 
-export default function Post({ packageData }) {
-    return (
-      <Layout>
-        <Head>
-          <title>{postData.title}</title>
-        </Head>
-        <article>
-          <h1 className={utilStyles.headingXl}>{postData.title}</h1>
-          <div className={utilStyles.lightText}>
-            <Date dateString={postData.date} />
-          </div>
-          <div dangerouslySetInnerHTML={{ __html: postData.contentHtml }} />
-        </article>
-      </Layout>
-    );
-  }
+// export default function Post({ packageData }) {
+//     return (
+//       <Layout>
+//         <Head>
+//           <title>{postData.title}</title>
+//         </Head>
+//         <article>
+//           <h1 className={utilStyles.headingXl}>{postData.title}</h1>
+//           <div className={utilStyles.lightText}>
+//             <Date dateString={postData.date} />
+//           </div>
+//           <div dangerouslySetInnerHTML={{ __html: postData.contentHtml }} />
+//         </article>
+//       </Layout>
+//     );
+//   }
+
+  
+export default function Post() {
+  return (
+    <Layout>
+      <Head>
+        <title>test</title>
+      </Head>
+      <article>
+        <h1 className={utilStyles.headingXl}>title</h1>
+        <div className={utilStyles.lightText}>
+        </div>
+      </article>
+    </Layout>
+  );
+}
