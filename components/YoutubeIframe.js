b85d69ee52aa6c9e57cfdd513645c48572ac048a
Tal <rozenblattal@gmail.com>
2022-07-10 17:01:26 +0300

    added toc to /sample page

--- components/YoutubeIframe.js
+++ components/YoutubeIframe.js
@@ -1,7 +1,7 @@
 
 
 const YoutubeIframe = ({ src }) => (
-    <iframe width="560" height="315" src={src} title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+    <iframe width="560" height="315" src={src} title="YouTube video player" frameBorder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowFullScreen></iframe>
   );
 
 export default YoutubeIframe;
