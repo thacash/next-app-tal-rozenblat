79b2029cc4d0a1d6af1591187e019ade4198c6ab
Tal Rozenblat <rozenblattal@gmail.com>
2022-10-24 15:20:36 +0300

    new about page design and new font

--- components/HomepageHeaderCard.js
+++ components/HomepageHeaderCard.js
@@ -4,7 +4,7 @@ import styles from "../styles/HomepageHeaderCard.module.css";
 export default function HomepageHeaderCard() {
   return (
     <div className={styles.card}>
-      <h1 className={styles.h1}>Pave you way to financial freedom</h1>
+      <h1 className={styles.h1}>Pave your way to financial freedom</h1>
       <p className={styles.p}>
         Here in Cash Manage, we have the tools you need to have a clear view on your finances, and how you could achieve financial freedom sooner.
       </p>
--- pages/about.js
+++ pages/about.js
@@ -13,13 +13,18 @@ export default function About() {
       </Head>
       {/* <HomePageBackground> */}
        <div className={styles.about}>
-          <h1 className={styles.h1}>about us</h1>
+       <h1 className={styles.h1}>About us</h1>
+
           <div className={styles.p}>
 
             <p>Cash manage is here to help you manage your finances, by combining your financial information into a simple and easy to understand collection of data.</p>
+            <p>Cash Manage is here to give you the tools to achieve financial freedom.</p>
+            <p>With our "Fire" calculator, abbreviation for "Financial independence, retire early", you can enter simple financial data about yourself and get a rough estimate as to when you can retire.</p>
+
             <p>By logging your income and expenses, you can see your spending habits, with that knowledge, you can make informed decisions regarding your spendings.  </p>
 
           </div>
+
        </div>
       {/* </HomePageBackground> */}
     </Layout>
